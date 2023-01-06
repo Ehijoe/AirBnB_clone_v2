0d9f5ee1903aabb5a24ae5a4a0f20ec69cbb7d82
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-06 18:04:53 +0100

    Add documentation for dev database setup script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,16 @@
+-- Setup the dev database and user
+
+-- Create the dev database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-  IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create the dev user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Grant the user the privilege to use all databases
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
+-- Grant the user the privilege to query the performance_schema database
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant the user all privileges on the hbnb_dev_db database
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
