bf1cbc503cc4d1d73e8dfc00e0f06f69224e3653
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-23 21:03:08 +0100

    Add fabric script for deployment

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Deploy the hbnb static site."""
+import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Pack the contents of web_static in an archive."""
+    time = datetime.datetime.now()
+    time = time.strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{time}.tgz"
+    print(archive_name)
